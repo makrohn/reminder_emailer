eaf85ff1ffebb0f711e74bbbd814eb75107d7c31
Matthew Krohn <makrohn@gmail.com>
2016-05-16 08:46:23 -0400

    Ready for testing

--- management/commands/send_emails.py
+++ management/commands/send_emails.py
@@ -1,5 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+"""
+Script to send emails to manager of employees who have separated,
+warning them that the employee's Google account is pending deletion
+"""
 
+from django.core.management.base import BaseCommand, CommandError
 import smtplib
 from email.mime.text import MIMEText
 from emailer import settings
@@ -8,12 +12,12 @@ from emailer.models import separatedEmployee
 
 
 def send_email(name, email, time):
+    """Send an email to a manager regarding their employee"""
     days_remaining = settings.deletion_day - time
     if days_remaining == 1:
         time_string = "tomorrow"
     else:
         time_string = "in %s days" % days_remaining
-#    print time_string
     msg = MIMEText(
         "TEST MESSAGE Hi! The Google account for " +
         name +
@@ -23,7 +27,6 @@ def send_email(name, email, time):
     msg['Subject'] = "Pending Google account deletion for " + name
     msg['From'] = settings.from_address
     msg['To'] = email
-    print msg
     s = smtplib.SMTP(settings.smtp_server)
     s.sendmail(
         settings.from_address,
@@ -33,20 +36,21 @@ def send_email(name, email, time):
 
 
 def process_matching_employees(days):
+    """Find employees who left a certain number of days ago"""
     today = datetime.date.today()
     warning_date = today - datetime.timedelta(days)
     alerts = separatedEmployee.objects.filter(set_date=warning_date)
-    for employee in alerts:
-        send_email(
-            employee.name,
-            employee.manager_email,
-            days,
-            )
+    return alerts
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for key in settings.reminder_dates:
             if settings.reminder_dates[key] < settings.deletion_day:
-                print key, settings.reminder_dates[key]
-                process_matching_employees(settings.reminder_dates[key])
+                alerts = process_matching_employees(settings.reminder_dates[key])
+                for employee in alerts:
+                    send_email(
+                        employee.name,
+                        employee.manager_email,
+                        settings.reminder_dates[key],
+                        )
